f2b1e93f2b1edc4d69e37c42857bce655144aa2a
LynneDC <roselinedc1308@gmail.com>
2023-06-06 17:14:21 +0200

    print the last digit of a number

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,6 +6,6 @@ digit = abs(number) % 10
 if digit % 10 > 5 :
 	print("{} {} is {}  and is greater than 5 ".format(string, number, digit, end=""))
 elif digit == 0:
-	print("{}  {} is {}and is 0".format(string, number, digit, end="") )
+	print("{}  {} is {} and is 0".format(string, number, digit, end="") )
 else:
 	print("{} {} is {} and is less than 6 and not 0".format(string, number, digit, end=""))
