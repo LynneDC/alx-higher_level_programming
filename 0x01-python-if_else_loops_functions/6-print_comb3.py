03d6d9417499eb967d9f5b691ef014f444f67235
LynneDC <roselinedc1308@gmail.com>
2023-06-07 17:15:19 +0200

    gets the combination of the small numbers only

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+for num in range(10):
+    for num1 in range(1, 10):
+        if num < num1:
+            continue
+        if num == 9 and num1 == 8:
+           print("{}{}".format(num, num1))
+           break
+        if num != num1:
+           print("{:02d} {:02d}".format(num, num1), end=" ")
