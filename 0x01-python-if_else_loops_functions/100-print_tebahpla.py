984ef1d25612e90ddc5de3602c7d3bc085983dd9
LynneDC <roselinedc1308@gmail.com>
2023-06-09 16:14:46 +0200

    prints all ascii leters with upper case on every odd positioned latter

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
-for letter in range(97, 124 -1):
+for letter in range(122, 96, -1):
     if letter % 2 == 0:
         print("{}".format(chr(letter)), end="")
     else:
-        print("{}".format(chr(letter)).upper(), end="") 
+        print("{}".format(chr(letter)).upper(), end="")
