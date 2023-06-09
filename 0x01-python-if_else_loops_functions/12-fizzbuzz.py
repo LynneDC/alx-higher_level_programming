bb0355c5e14345df045d4c6fe901e3b5b74f0c75
LynneDC <roselinedc1308@gmail.com>
2023-06-09 12:44:58 +0200

    prints numbers and string on every multple of 3 and or 5

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+str1 = "Fizz "
+str2 = "Buzz "
+str3 = "FizzBuzz"
+def fizzbuzz():
+    for num in range(1, 100):
+#    print("{}".format(num))
+        if num % 3 == 0 and num % 5 == 0:
+            print("{}  ".format(str3), end=" ")
+        elif num % 3 == 0:
+            print("{}  ".format(str1), end=" ")
+        elif num % 5 == 0:
+            print("{}  ".format(str2), end=" ")
+        else:
+            print("{}  ".format(num), end="")
