9c10a1280afff00dc22a44de6755daf30e774cb8
LynneDC <roselinedc1308@gmail.com>
2023-06-17 18:49:22 +0200

    task 2

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def uniq_add(my_list=[]):
+    unique_list = []
+    for item in my_list:
+        if item not in unique_list:
+            unique_list.append(item)
+    return sum(unique_list)
