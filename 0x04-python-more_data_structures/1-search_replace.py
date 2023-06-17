527c58dae5dc87f1e0f27fdf9d28afb24659935c
LynneDC <roselinedc1308@gmail.com>
2023-06-17 18:44:49 +0200

    task 2

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,11 +2,10 @@
 
 def search_replace(my_list, search, replace):
     new_list = []
-    if my_list != []:	
+    if my_list != []:
         for item in my_list:
             if item == search:
                 new_list.append(replace)
             else:
                 new_list.append(item)
-        return new_list
-
+    return new_list
