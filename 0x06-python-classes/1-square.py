eb7b7c170c5073a5661875649c779d05332079a0
LynneDC <roselinedc1308@gmail.com>
2023-06-27 15:50:39 +0200

    defines a class that has private attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""defines a class called square """
+
+
+class Square:
+    """define and initilize using init"""
+    def __init__(self, size):
+        """assign value to an object"""
+        self.__size = size
