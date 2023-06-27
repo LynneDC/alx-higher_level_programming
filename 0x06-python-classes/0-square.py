e7b856471a9f7460b9198b32bf1783ab657e3c73
LynneDC <roselinedc1308@gmail.com>
2023-06-27 15:18:33 +0200

    prints an empty class

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -2,5 +2,9 @@
 
 """a function that defines  an empty square class """
 
+"""declare a class with square funcion name"""
 class Square:
+
+	"""silent classthat is empty and does nothing"""
     pass
+    """ this code is going to be silenced for future use"""
