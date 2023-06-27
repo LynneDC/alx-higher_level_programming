6284244ce55bc631b4f65be15f5f75458c8131fb
LynneDC <roselinedc1308@gmail.com>
2023-06-27 16:51:07 +0200

    to edit

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -8,7 +8,7 @@ class Square:
         """handle errors"""
         try:
             """check is size is not digit"""
-            if not isistance(size, int):
+            if type(size) is not int:
                 """print out type error message because its expecting int"""
                 raise TypeError("size  mustbe an integer")
             """check if size is less tha 0"""
@@ -19,5 +19,6 @@ class Square:
                 """
                 raise ValueError("size must be >= 0")
 
-            """assign value to an object"""
-            self.__size = size
+	    else:
+	        """assign value to an object"""
+                self.__size = size
