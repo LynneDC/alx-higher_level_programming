e2618c32871cfd31a8c56f175f99c01395d01c55
LynneDC <roselinedc1308@gmail.com>
2023-06-27 22:23:40 +0200

    task done

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -34,13 +34,13 @@ class Square:
 
     @position.setter
     def position(self, value):
-        if (not isinstance(value, tuple) or
-            len(value) != 2 or
-            not all(isinstance(num, int) for num in value) or
-            not all(num >= 0 for num in value)):
+        if (not isinstance(value, tuple) or len(value) != 2 or
+                not all(isinstance(num, int) for num in value) or
+                not all(num >= 0 for num in value)):
+
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
-           self.__position = value
+            self.__position = value
 
     def area(self):
         """Return the current area of the square."""
