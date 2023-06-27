6d39472c7e8e8e18efa2c7b542a17d976ba941e9
LynneDC <roselinedc1308@gmail.com>
2023-06-27 08:23:32 -0700

    new

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -7,19 +7,19 @@ class Square:
     """define and initilize using init"""
     def __init__(self, size=0):
         """handle errors"""
-        try:
-            """check is size is not digit"""
-            if type(size) is not int:
-                """print out type error message because its expecting int"""
-                raise TypeError("size  mustbe an integer")
-            """check if size is less tha 0"""
-            if size < 0:
-                """
-                raise a valueerror message because
-                the minimum value should be of size 0
-                """
-                raise ValueError("size must be >= 0")
+       
+        """check is size is not digit"""
+        if type(size) is not int:
+            """print out type error message because its expecting int"""
+            raise TypeError("size  mustbe an integer")
+        """check if size is less tha 0"""
+        if size < 0:
+            """
+            raise a valueerror message because
+            the minimum value should be of size 0
+            """
+            raise ValueError("size must be >= 0")
 
             
-           ""assign value to an object"""
-            self.__size = size
+        """assign value to an object"""
+        self.__size = size
