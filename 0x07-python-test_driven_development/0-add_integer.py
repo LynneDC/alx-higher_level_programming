12aa3e4846ee7414fec2c6725ab80bfece93d09e
LynneDC <roselinedc1308@gmail.com>
2023-07-05 05:10:49 -0700

    to update

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+def add_integer(a, b=98):
+    """
+    Adds two integers.
+
+    Args:
+        a (int or float): The first integer or float value to be added.
+        b (int or float, optional): The second integer or float value to be added. Defaults to 98.
+
+    Raises:
+        TypeError: If `a` or `b` is not an integer or float.
+
+    Returns:
+        int: The sum of `a` and `b`, casted to an integer.
+
+    Examples:
+        >>> add_integer(5, 3)
+        8
+        >>> add_integer(2.5, 4.7)
+        6
+        >>> add_integer(10)
+        108
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer or float")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer or float")
+
+    a = int(a)
+    b = int(b)
+
+    return a + b
+
