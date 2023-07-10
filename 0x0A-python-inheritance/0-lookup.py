a6110fa91e4e618f798f7037914570661ca69cb8
LynneDC <roselinedc1308@gmail.com>
2023-07-10 12:41:00 +0200

    return all attribute pfobject using dir

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# 0-lookup
+
+def lookup(obj):
+    """
+    returns all the attributes of an obj
+    uses dir to get attribute and property of object
+    """
+    return dir(obj) 
