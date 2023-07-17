6551f349325baac2ad00117ff82cf8ec20ac1924
LynneDC <roselinedc1308@gmail.com>
2023-07-18 00:19:14 +0200

    removed

--- TDD/sum/tests/test_sum/test_another_sum.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3 
-
-from sum.another_sum import another_sum
-
-
-def test_another_sum():
-    assert another_sum(3, 2) == 5
--- classes-prac/oop_init.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-
-class Person:
-   def __init__(self, name):
-        self.name = name
-   def say_hi(self):
-        print("Hello, my name is ", self.name)
-Person("Roseline").say_hi()	
-
