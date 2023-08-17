55412d90d7cb5186075b8d0523becee7c0166bdf
LynneDC <roselinedc1308@gmail.com>
2023-08-17 12:27:12 +0200

    display on top 3

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,3 +1,3 @@
 -- Import in hbtn_0c_0 database this table dump
 -- displays top 3 of cities tempe during July and Aug ordered by temp desc
-SELECT city, AVG(value) AS avg_temp FROM temperatures WHERE month=7 OR month=8 GROUP BY avg_temp DESC LIMIT 3;
+SELECT city, AVG(value) AS avg_temp FROM temperatures WHERE month=7 OR month=8 GROUP BY city ORDER BY avg_temp DESC LIMIT 3;
