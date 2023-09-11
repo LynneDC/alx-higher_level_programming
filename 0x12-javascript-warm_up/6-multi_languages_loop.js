c1903a5c1bd25fae444ac3383e5262dcfbf6e8a5
LynneDC <roselinedc1308@gmail.com>
2023-09-11 17:33:23 +0200

    array for multi lines

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+// Define an array of strings
+// Loop through the array and print each line
+const lines = [
+  'C is fun',
+  'Python is cool',
+  'JavaScript is amazing'
+];
+
+for (let i = 0; i < lines.length; i++) {
+  console.log(lines[i]);
+}
