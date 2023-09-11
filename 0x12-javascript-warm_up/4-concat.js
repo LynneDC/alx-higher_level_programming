0a29da55f1fc5280f884d0be090b93ec5f3e0f0f
LynneDC <roselinedc1308@gmail.com>
2023-09-11 14:31:36 +0200

    js concatination

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
-
 // concatenate and print the args
-// add is to concatenate and prints out 
+// add is to concatenate and prints out
 // arguments are in cmd
-  console.log(process.argv[2] + ' is ' + process.argv[3]);
+console.log(process.argv[2] + ' is ' + process.argv[3]);
