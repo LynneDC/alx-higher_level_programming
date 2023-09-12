665d7c862375cd79c716329411d526c426feb19e
LynneDC <roselinedc1308@gmail.com>
2023-09-12 12:36:31 +0200

    update function

---
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+// function that increment int value
+const myObject = {
+  type: 'object',
+  value: 12
+}
+console.log(myObject);
+myObject.incr = function () {
+  this.value++;
+};
+myObject.incr();
+console.log(myObject);
+
+myObject.incr();
+console.log(myObject);
+
+myObject.incr();
+console.log(myObject);
