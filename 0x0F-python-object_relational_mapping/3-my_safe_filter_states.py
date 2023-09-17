e24983655faa1452a4d0f8fc56d738c34ce52f24
LynneDCe <roselinedc1308@gmail.com>
2023-09-17 15:34:38 +0200

    takes argu and print its values in safe sql injection

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""states models
+  taskes a value arguments and display values
+  in safe injection mode
+   in state table
+"""
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
+
+    db_host = "localhost"
+    db_user = sys.argv[1]  # "your_username"
+    db_password = sys.argv[2]  # "your_password"
+    db_name = sys.argv[3]  # "your_database_name"
+    port = 3306
+
+    db = MySQLdb.connect(
+        host=db_host, user=db_user, passwd=db_password, db=db_name, port=port
+    )
+    cursor = db.cursor()
+
+    cursor.execute("SELECT * FROM states WHERE name=%s\
+                   ORDER BY states.id ASC", (sys.argv[4],))
+
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    db.close()
